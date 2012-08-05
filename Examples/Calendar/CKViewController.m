ab178d718f88cefd5a3d87ad2971f6812994a476
Jason Kozemczak <jason.kozemczak@gmail.com>
2012-08-05 10:04:04 -0500

    Updated example to show how delegate callbacks can be used.

--- Examples/Calendar/CKViewController.m
+++ Examples/Calendar/CKViewController.m
@@ -1,24 +1,33 @@
+#import <CoreGraphics/CoreGraphics.h>
 #import "CKViewController.h"
 #import "CKCalendarView.h"
 
-@interface CKViewController ()
+@interface CKViewController () <CKCalendarDelegate>
+@property(nonatomic, strong) UILabel *dateLabel;
+
 
 @end
 
 @implementation CKViewController
+@synthesize dateLabel = _dateLabel;
+
 
 - (id)init {
     self = [super init];
     if (self) {
         CKCalendarView *calendar = [[CKCalendarView alloc] initWithStartDay:startMonday];
+        calendar.delegate = self;
         
         NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
         [dateFormatter setDateFormat:@"dd/MM/yyyy"];
         calendar.selectedDate = [dateFormatter dateFromString:@"18/07/2012"];
         calendar.minimumDate = [dateFormatter dateFromString:@"09/07/2012"];
         calendar.maximumDate = [dateFormatter dateFromString:@"29/07/2012"];
-        calendar.frame = CGRectMake(10, 10, 300, 470);
+        calendar.frame = CGRectMake(10, 10, 300, 320);
         [self.view addSubview:calendar];
+
+        self.dateLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(calendar.frame) + 4, self.view.bounds.size.width, 24)];
+        [self.view addSubview:self.dateLabel];
 
         self.view.backgroundColor = [UIColor whiteColor];
     }
@@ -45,5 +54,13 @@
         return YES;
     }
 }
+
+#pragma mark -
+#pragma mark - CKCalendarDelegate
+
+- (void)calendar:(CKCalendarView *)calendar didSelectDate:(NSDate *)date {
+    self.dateLabel.text = [date description];
+}
+
 
 @end
